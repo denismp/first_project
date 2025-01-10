aa605e6a41eb4e3fc582951544fe9a5803625680
Denis Putnam <denisputnam@verizon.net>
2025-01-10 18:05:36 -0500

    first view.py, settings.py, and templates.

--- first_project/settings.py
+++ first_project/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+print(TEMPLATE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
